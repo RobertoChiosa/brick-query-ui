484dd7486f5c2cb474d8aa067720a5da8aab2b05
RobertoChiosa <roberto.chiosa@gmail.com>
2022-10-10 16:47:30 +0200

    Added README.md and ttl json formatter

---
+++ README.md
@@ -0,0 +1,5 @@
+# brick-portable-app
+
+
+![mortar-paper](./static/img/app.png "Example of timeseries from query on timescald db and brick model")
+
--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, request
-
+import rdflib
+from flask import Flask, render_template, request, jsonify
+import json
 app = Flask(__name__, static_url_path='/static')
 
 
 @app.route("/")
 def main():
     # initialize the query editor with a simple query
-    query_editor_text = """SELECT ?x WHERE {
-    ?x a brick:
+    query_editor_text = """SELECT ?ahu ?sat  WHERE {
+    ?ahu a brick:AHU .
+  	?ahu  brick:hasPoint  ?sat .
+  	?sat  a  brick:Supply_Air_Temperature_Sensor .
 }"""
     return render_template('index.html', query_editor_text=query_editor_text)
 
@@ -16,7 +19,13 @@ def main():
 def query():
     if request.method == 'POST':
         query_editor_text = request.form['query_editor_text']  # keep the query on editor
-        query_result_text = request.form['query_editor_text']  # process query
+
+        g = rdflib.Graph()
+        g.parse('brick-query-ui/static/ttl/dbc_brick_expanded.ttl', format='ttl')
+        res = g.query(request.form['query_editor_text'])
+        res_dict = [dict(item) for item in res.bindings]
+
+        query_result_text = json.dumps(res_dict, indent=1)
 
     return render_template('index.html', query_editor_text=query_editor_text, query_result_text=query_result_text)
 
